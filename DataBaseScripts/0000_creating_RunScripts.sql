a528dcb4b188cd251f9b11b06772a4b6f4d3985b
Yoannis P. Guerra <ypguerra80@gmail.com>
2018-10-05 15:00:10 -0500

    database

--- DataBaseScripts/0000_creating_RunScripts.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[RunScripts]
-(
-	[Id] INT NOT NULL IDENTITY,
-    	[Script] VARCHAR(255) NOT NULL, 
-    	[Date] DATETIME NOT NULL
-)
--- DataBaseScripts/0002_populating_table_AccountType.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO [dbo].[AccountType] ([Id], [AccountType], [Passive]) VALUES (0, N'Asset', 1)
-INSERT INTO [dbo].[AccountType] ([Id], [AccountType], [Passive]) VALUES (1, N'Liability', 0)
-INSERT INTO [dbo].[AccountType] ([Id], [AccountType], [Passive]) VALUES (2, N'Equity', 0)
-INSERT INTO [dbo].[AccountType] ([Id], [AccountType], [Passive]) VALUES (3, N'Income', 0)
-INSERT INTO [dbo].[AccountType] ([Id], [AccountType], [Passive]) VALUES (4, N'Expense', 1)
-INSERT INTO [dbo].[AccountType] ([Id], [AccountType], [Passive]) VALUES (5, N'Contingency Asset', 1)
-INSERT INTO [dbo].[AccountType] ([Id], [AccountType], [Passive]) VALUES (6, N'Contingency Liability', 0)
-
-Insert into [dbo].[RunScripts] (Script, Date) Values ('0002_populating_table_AccountType', getdate())
